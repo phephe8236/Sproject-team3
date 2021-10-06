f34e5fc7dc7a865a8988ffbed26f4c03f1465774
YUJEONG <YUJEONG@yujeong>
2021-10-06 19:45:50 +0900

    한글 수정22

--- Sproject_team3/src/main/java/projectDao/test.java
+++ Sproject_team3/src/main/java/projectDao/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ÇÑ±ÛÀÔ·Â asdf");
+		System.out.println("ì•ˆë…•ã…Žã…Žã…Ž asdf");
 	}
 
 }
